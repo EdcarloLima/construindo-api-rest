9e2f90b3c6e567d76a4653896dc7e62d0b9b3cb0
Edcarlo Lima <edcarlo.lima@institutoprosaber.com.br>
2020-07-09 09:49:59 -0300

    Primeiro commit - inicio do projeto api

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function (Request $request){
+    //dd($request->headers->get('')); chave get para pegar informação no cabeçalho
+
+    $response =  new \Illuminate\Http\Response(json_encode(['msg'=>'Minha primeira resposta da api']));
+    $response->header('Content-Type','application/json');
+    return $response;
+   //return ['msg'=>'Minha primeira resposta da api']; já retorna como application/json
+});
